068d4d7954724bf53f0f2ca647dde8997fcffc44
jonathan twaddell <jon@arclocal.com>
2014-09-22 17:30:30 -0700

    updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seek::Application.config.secret_token = '47e92f42bbb1e420a59f8861b109272b349331215ebe8639224d57d2b203a9bb934663f69c2c8d3c4e66a44f0f80503cf9874e2ea683df6d5a5e5688fbb94f92'
