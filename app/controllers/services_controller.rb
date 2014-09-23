e905add12911303db95f2e75b8aba7b975751cc3
jonathan twaddell <jon@arclocal.com>
2014-09-22 19:23:42 -0700

    added client_id

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,5 +2,7 @@ class ServicesController < ApplicationController
 
  def instagram
    puts "within instagram"
+   auth_hash = request.env['omniauth.auth']
+   render :text => auth_hash.inspect
  end
 end
