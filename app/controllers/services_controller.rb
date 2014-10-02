e5c91f2455c6eec1fa97400c825e9ec92c633dd2
Petro Koriakin <pkor@softserveinc.com>
2014-10-02 03:58:46 +0300

    trying to fix broken login

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,9 +1,29 @@
 class ServicesController < ApplicationController
 
- def instagram
-   puts "within instagram"
-   auth_hash = request.env['omniauth.auth']
-   User.create! ig_name:"My fuckin name!"
-   render :text => auth_hash.inspect
- end
+  def instagram
+    @message = begin
+      RestClient.post 'https://api.instagram.com/oauth/access_token', parameters
+      sign_in User.find_or_create_by_instagram JSON.parse(response.body)
+      'successfully signed in'
+    rescue => e
+      e.response.body
+    end
+    render 'index/root'
+  end
+
+  private
+
+  def url_for_redirect
+    "http://#{request.host}#{request.port == 3000 ? ':3000' : ''}"
+  end
+
+  def parameters
+    {
+      client_id: IG_SECRETS[Rails.env][:id],
+      client_secret: IG_SECRETS[Rails.env][:key],
+      grant_type: 'authorization_code',
+      redirect_uri: url_for_redirect + '/arc/auth/instagram/callback',
+      code: params[:code]
+    }
+  end
 end
